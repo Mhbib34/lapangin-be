85e7d6d8cecd15f17e237a9c1a62a18ae78b1929
Mhbib34 <mhabib34official@gmail.com>
2025-07-22 11:23:29 +0700

    remove duplicate logic in field service

--- src/service/field-service.ts
+++ src/service/field-service.ts
@@ -10,6 +10,21 @@ import { prismaClient } from "../config/database";
 import { ResponseError } from "../error/response-error";
 
 export class FieldService {
+  static async checkFieldMustExist(fieldId: string) {
+    const field = await prismaClient.field.findUnique({
+      where: {
+        id: fieldId,
+      },
+      include: {
+        category: true,
+      },
+    });
+
+    if (!field) {
+      throw new ResponseError(404, "Field not found");
+    }
+    return field;
+  }
   static async create(request: CreateFieldRequest): Promise<FieldResponse> {
     const fieldRequest = Validation.validate(FieldValidation.CREATE, request);
     const field = await prismaClient.field.create({
@@ -38,13 +53,7 @@ export class FieldService {
     request: UpdateFieldRequest
   ): Promise<FieldResponse> {
     const fieldRequest = Validation.validate(FieldValidation.UPDATE, request);
-    const findField = await prismaClient.field.findUnique({
-      where: {
-        id: fieldId,
-      },
-    });
-
-    if (!findField) throw new ResponseError(404, "Field not found");
+    await this.checkFieldMustExist(fieldId);
 
     const fieldUpdate = await prismaClient.field.update({
       where: {
@@ -71,27 +80,11 @@ export class FieldService {
   }
 
   static async get(id: string): Promise<FieldResponse> {
-    const field = await prismaClient.field.findUnique({
-      where: {
-        id,
-      },
-      include: {
-        category: true,
-      },
-    });
-    if (!field) throw new ResponseError(404, "Field not found");
+    const field = await this.checkFieldMustExist(id);
     return toFieldResponse(field, field.category);
   }
   static async remove(id: string): Promise<FieldResponse> {
-    const field = await prismaClient.field.findUnique({
-      where: {
-        id,
-      },
-      include: {
-        category: true,
-      },
-    });
-    if (!field) throw new ResponseError(404, "Field not found");
+    const field = await this.checkFieldMustExist(id);
 
     await prismaClient.field.delete({
       where: {
